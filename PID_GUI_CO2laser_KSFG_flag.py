1f47134d32804e194e481025db6294511ad67238
Hiroki Takahashi <hiroki.t.hashi@gmail.com>
2022-08-18 10:25:25 +0900

    Can see in repo, but is not showing as added to git in local file

--- PID_GUI_CO2laser_KSFG_flag.py
+++ PID_GUI_CO2laser_KSFG_flag.py
@@ -81,9 +81,11 @@ class MainWindow(baseClass):
         self.ui.save_button.clicked.connect(self.save_data)
         self.ui.load_button.clicked.connect(self.load_data)
         self.ui.close_button.clicked.connect(self.close_gui)
+        self.ui.laser_low_button.clicked.connect(self.laser_low)
         self.ui.sp_doubleSpinBox.valueChanged.connect(self.write_power)
         self.ui.int_SpinBox.valueChanged.connect(self.start_plot_timer)
-        self.ui.inf_checkBox.stateChanged.connect(self.start_plot_timer)        
+        self.ui.inf_checkBox.stateChanged.connect(self.start_plot_timer)
+        self.ui.dt_SpinBox.valueChanged.connect(self.start_PID_timer)        
         self.interval = self.ui.int_SpinBox.value()
         self.win = self.ui.win_SpinBox.value()
         self.dt = self.ui.dt_SpinBox.value()
@@ -91,7 +93,7 @@ class MainWindow(baseClass):
 
         self.ui.graphwidget.setBackground('w')
         styles = {'color':'r', 'font-size':'20px'}
-        self.ui.graphwidget.setLabel('left', 'Power (mW)', **styles)
+        self.ui.graphwidget.setLabel('left', 'Power (W)', **styles)
         self.ui.graphwidget.setLabel('bottom', 'Time (s)', **styles)
         pen = pg.mkPen(color=(0, 0, 255), width = 2)
         pen_v = pg.mkPen(color=(0, 255, 0), width = 2)
@@ -102,7 +104,6 @@ class MainWindow(baseClass):
         self.y_v = list(np.zeros(1))
         self.data_line_v = self.ui.graphwidget.plot(self.x, self.y_v, pen=pen_v)
         self.data_line = self.ui.graphwidget.plot(self.x, self.y, pen=pen)
-        self.PWM = 2
         #self.write_power(self.setpoint)
         self.show()  
         self.start_PID_timer()
@@ -112,27 +113,6 @@ class MainWindow(baseClass):
         self.tlPM.measPower(byref(power_c))
         Power = power_c.value
         return (float(Power))
-    
-   # def read_pow(self): #Function is called every 'dt' ms and provides input for PID and plotting
-      #  flag = self.task.read()
-       # self.clock[1] = perf_counter()
-       # time_int = self.clock[1]-self.clock[0]
-       # self.counter[1] = self.counter[0]+time_int
-       # time = self.counter[1]
-       # if self.ui.shootBox.isChecked() == False:
-        
-        #    self.pow = self.readTLPM()
-        #    self.buff_pow = [time,self.pow]
-        #    if self.ui.PID_checkBox.isChecked() == True:
-       #         p_out, self.ERR, self.PID = self.PID_run(time_int, self.pow)
-        #        self.write_power(p_out)
-        #    else:
-                
-          #          self.buff_pow = [time,self.pow]
-             #   else:
-
-        #self.clock[0] = self.clock[1]
-        #self.counter[0] = self.counter[1]
 
     def read_pow(self): #Function is called every 'dt' ms and provides input for PID and plotting
         flag = self.task.read()
@@ -140,23 +120,21 @@ class MainWindow(baseClass):
         time_int = self.clock[1]-self.clock[0]
         self.counter[1] = self.counter[0]+time_int
         time = self.counter[1]
-        if flag > 1:
+        if flag < 1 & self.ui.shootBox.isChecked() == True:
+            self.buff_pow = [time,self.pow]
+        else:
             self.pow = self.readTLPM()
             self.buff_pow = [time,self.pow]
             if self.ui.PID_checkBox.isChecked() == True:
                 p_out, self.ERR, self.PID = self.PID_run(time_int, self.pow)
                 self.write_power(p_out)
-        else:
-            #self.pow = self.readTLPM()
-            self.buff_pow = [time,self.pow]
-            print('shooting')
         self.clock[0] = self.clock[1]
         self.counter[0] = self.counter[1]
 
     def start_PID_timer(self):#Start a second timer used for the plotting, usually required to be different from the PID interval 'dt'
         self.clock[0] = perf_counter()
         self.timer.timeout.connect(self.read_pow)
-        self.timer.start(self.dt)
+        self.timer.start(self.dt)        
 
     def start_plot_timer(self):#Start a second timer used for the plotting, usually required to be different from the PID interval 'dt'
         self.int = self.ui.int_SpinBox.value()
@@ -170,11 +148,16 @@ class MainWindow(baseClass):
             self.timer2.stop()
          
     def write_power(self, pow_in):#Command to write voltage from output device, to be used as the feedback signal
-        self.PWM = (pow_in+0.384)/0.2684
+        self.PWM = (pow_in+0.42889)/0.33463
         str1 = 'FUNc:SQU:DCYC +'
         str2 = str(self.PWM)
         str_out = str1+str2
         self.inst.write(str_out)
+
+    def laser_low(self):
+        self.write_power(0.1)
+        self.ui.sp_doubleSpinBox.setProperty("value", 0.1)
+        self.ui.PID_checkBox.setChecked(False)
 
     def PID_run(self, d_t, pow_in):
         setpoint = self.ui.sp_doubleSpinBox.value()
@@ -187,8 +170,8 @@ class MainWindow(baseClass):
         self.Der = (self.Error[1] - self.Error[0])/d_t #derivative of the error, D
         correction = K*KP*self.Error[1] + K*KI*self.Int[1]+ K*KD*self.Der
         new_power = correction + pow_in
-#        p_out = self._clamp(new_power, (0, (14*0.2684)-0.384))
-        p_out = self._clamp(new_power, (0, 2.5))
+        p_out = self._clamp(new_power, (0, (14*0.33463)-0.42889))
+        #p_out = self._clamp(new_power, (0, 2.5))
         self.Error[0] = self.Error[1]# Pass new values for next reading
         self.Int[0] = self.Int[1]
         return p_out, self.Error[1], correction
@@ -212,33 +195,24 @@ class MainWindow(baseClass):
         self.ui.v_out_disp.display(self.PWM)
 
     def update_plot_data(self, time, pow_in, error): #Updates the plot with new values of time and pow_in. Plot resizes depending on 'window'
-        #self.win = round(self.ui.win_SpinBox.value()/self.ui.int_SpinBox.value())
-        #print(pow_in)
-        self.win = self.ui.win_SpinBox.value()
+        self.win = round(self.ui.win_SpinBox.value()/(0.001*self.ui.int_SpinBox.value()))
         diff = self.win-len(self.x)
-        if self.ui.inf_checkBox.isChecked() == True:
-            self.x.append(time)  # Add a new value 1 higher than the last.
-            self.y.append(pow_in)  # Add a new power value.
-            self.y_v.append(error)  # Add a new power value.
-        else:
+        self.x.append(time)  # Add a new value 1 higher than the last.
+        self.y.append(pow_in)  # Add a new power value.
+        self.y_v.append(error)  # Add a new power value.
+        if self.ui.inf_checkBox.isChecked() == False:
             if diff == 0:
-                self.x = self.x[1:]  # Remove the first y element.
-                self.x.append(time)  # Add a new value 1 higher than the last.
-                self.y = self.y[1:]  # Remove the first
-                self.y.append(pow_in)  # Add a new power value.
+                #print(self.win)
+                #print(len(self.x))
+                self.x = self.x[1:]  # Remove the first x element.
+                self.y = self.y[1:]  # Remove the first y element.
                 self.y_v = self.y_v[1:]  # Remove the first
-                self.y_v.append(error)  # Add a new power value.
-            elif diff > 0:
-                self.x.append(time)  # Add a new value 1 higher than the last.
-                self.y.append(pow_in)  # Add a new power value.
-                self.y_v.append(error)  # Add a new power value.
             elif diff < 0:
-                self.x = self.x[5:]  # Remove the first y element.
-                self.x.append(time)  # Add a new value 1 higher than the last.
-                self.y = self.y[5:]  # Remove the first
-                self.y.append(pow_in)  # Add a new power value.
+                #print(self.win)
+                #print(len(self.x))
+                self.x = self.x[5:]  # Remove the first 5 x elements.
+                self.y = self.y[5:]  # Remove the first 5 y elements.
                 self.y_v = self.y_v[5:]  # Remove the first
-                self.y_v.append(error)  # Add a new power value.
         if self.ui.PIDout_checkBox.isChecked() == True:
             self.data_line_v.setData(self.x, self.y_v)  # Update the data.self.data_line_v.setData(self.x,self.y)  # Update the data.
         else:
